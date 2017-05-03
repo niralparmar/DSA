0156b02ed23cd1dadc7936af26c4257e45426472
c0der_man <niral.parmar@hotmail.com>
2017-05-02 23:36:30 -0700

    LinkedList Test

--- cs/DSATest/DataStructureTest/LinkedListTest.cs
+++ cs/DSATest/DataStructureTest/LinkedListTest.cs
@@ -2,6 +2,7 @@
 {
     using DataStructure;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
 
     [TestClass]
     public class LinkedListTest
@@ -32,5 +33,219 @@
             Assert.AreEqual(10, list.Head.Next.Value);
             Assert.AreEqual(15, list.Tail.Value);
         }
+
+        /// <summary>
+        /// Check to see that the value of the Head node of the LinkedList is as expected.
+        /// </summary>
+        [TestMethod]
+        public void HeadTest()
+        {
+            LinkedList list = new LinkedList();
+            list.Add(10);
+
+            Assert.AreEqual(10, list.Head.Value);
+        }
+
+        /// <summary>
+        /// Check to see that the value of the Tail node of the LinkedList is as expected.
+        /// </summary>
+        [TestMethod]
+        public void TailTest()
+        {
+            LinkedList list = new LinkedList();
+            list.Add(10);
+
+            Assert.AreEqual(10, list.Tail.Value);
+        }
+        /// <summary>
+        /// Check to see that the Count property of the LinkedList returns the correct number.
+        /// </summary>
+        [TestMethod]
+        public void CountTest()
+        {
+            LinkedList list = new LinkedList();
+            list.Add(10);
+            list.Add(20);
+            Assert.AreEqual(2, list.Count);
+        }
+        /// <summary>
+        /// Check to see that calling RemoveFirst on LinkedList with only 1 node results in the expected behaviour.
+        /// </summary>
+        [TestMethod]
+        public void RemoveFirstOneItemInListTest()
+        {
+            LinkedList sll = new LinkedList();
+
+            sll.RemoveFirst();
+
+            Assert.AreEqual(0, sll.Count);
+            Assert.IsNull(sll.Head);
+            Assert.IsNull(sll.Tail);
+        }
+        /// <summary>
+        /// Check to see that when removing the only node in the list that the head and tail pointers are update correctly.
+        /// </summary>
+        [TestMethod]
+        public void RemoveOnlyNodeInListTest()
+        {
+            LinkedList list = new LinkedList();
+
+            list.Remove(12);
+
+            Assert.IsNull(list.Head);
+            Assert.IsNull(list.Tail);
+        }
+
+        /// <summary>
+        /// Check to see that when calling the RemoveFirst method on a LinkedListCollection with more than one node
+        /// results in the expected object state.
+        /// </summary>
+        [TestMethod]
+        public void RemoveFirstTest()
+        {
+            LinkedList list = new LinkedList();
+
+            list.AddFirst(40);
+            list.AddFirst(30);
+            list.AddFirst(20);
+            list.AddFirst(10);
+            list.Add(50);
+            list.RemoveFirst();
+            list.RemoveFirst();
+
+            Assert.AreEqual(30, list.Head.Value);
+            Assert.AreEqual(50, list.Tail.Value);
+            Assert.AreEqual(3, list.Count);
+        }
+
+        /// <summary>
+        /// Check to see that the correct value is returned when there is nothing to remove.
+        /// </summary>
+        [TestMethod]
+        public void RemoveFirstEmptyListTest()
+        {
+            LinkedList list = new LinkedList();
+
+            Assert.IsFalse(list.RemoveFirst());
+        }
+        /// <summary>
+        /// Check to see that the contains method returns the correct bool depending on whether the item is in the 
+        /// LinkedListCollection or not.
+        /// </summary>
+        [TestMethod]
+        public void ContainsTest()
+        {
+            LinkedList list = new LinkedList();
+            list.AddFirst(40);
+            list.AddFirst(30);
+            list.AddFirst(20);
+
+            Assert.IsTrue(list.Contains(20));
+            Assert.IsFalse(list.Contains(10));
+        }
+
+        /// <summary>
+        /// Check to see that the correct exception is raised when attempting to remove an item from an empty
+        /// LinkedListCollection.
+        /// </summary>
+        [TestMethod]
+        public void RemoveItemFromEmptyLinkedListCollection()
+        {
+            LinkedList list = new LinkedList();
+            
+            Assert.IsFalse(list.Remove(10));
+        }
+
+        /// <summary>
+        /// Check to see that Remove leaves the LinkedList in the correct state where the value of Remove
+        /// is equal to that of the head node.
+        /// </summary>
+        [TestMethod]
+        public void RemoveHeadItemTest()
+        {
+            LinkedList list = new LinkedList();
+            list.Add(10);
+            list.Add(20);
+            list.Add(30);
+
+            bool actual = list.Remove(10);
+
+            Assert.AreEqual(20, list.Head.Value);
+            Assert.AreEqual(30, list.Tail.Value);
+            Assert.AreEqual(30, list.Head.Next.Value);
+            Assert.AreEqual(2, list.Count);
+            Assert.IsTrue(actual);
+        }
+
+        /// <summary>
+        /// Check to see that Remove leaves the LinkedList in the correct state, when remove is any node but head or tail.
+        /// </summary>
+        [TestMethod]
+        public void RemoveMiddleItemTest()
+        {
+            LinkedList list = new LinkedList();
+            list.Add(10);
+            list.Add(20);
+            list.Add(30);
+
+            bool actual = list.Remove(20);
+
+            Assert.AreEqual(30, list.Head.Next.Value);
+            Assert.AreEqual(10, list.Head.Value);
+            Assert.AreEqual(30, list.Tail.Value);
+            Assert.AreEqual(2, list.Count);
+            Assert.IsTrue(actual);
+        }
+
+        /// <summary>
+        /// Check to see that Remove leaves the LinkedList in the correct state where the value of Remove
+        /// is equal to that of the tail node.
+        /// </summary>
+        [TestMethod]
+        public void RemoveTailItemTest()
+        {
+            LinkedList list = new LinkedList();
+            list.Add(10);
+            list.Add(20);
+            list.Add(30);
+            bool actual = list.Remove(30);
+
+            Assert.AreEqual(10, list.Head.Value);
+            Assert.AreEqual(20, list.Head.Next.Value);
+            Assert.AreEqual(20, list.Tail.Value);
+            Assert.AreEqual(2, list.Count);
+            Assert.IsNull(list.Tail.Next);
+            Assert.IsTrue(actual);
+        }
+        /// <summary>
+        /// Check to see that AddBefore a middle node results in the expected state of the LinkedList.
+        /// </summary>
+        [TestMethod]
+        public void AddBeforeMiddleNodeTest()
+        {
+            LinkedList list = new LinkedList();
+            list.Add(10);
+            list.Add(20);
+            list.Add(30);
+
+            list.AddBefore(list.Head.Next, 15);
+
+            Assert.AreEqual(15, list.Head.Next.Value);
+            Assert.AreEqual(20, list.Head.Next.Next.Value);
+            Assert.AreEqual(4, list.Count);
+        }
+
+        /// <summary>
+        /// Check to see that the correct exception is raised when calling AddBefore on a LinkedList with no nodes.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void AddBeforeEmptyListTest()
+        {
+            LinkedList list = new LinkedList();
+
+            list.AddBefore(list.Head, 10);
+        }
+
     }
 }
--- cs/DataStructure/LinkedList.cs
+++ cs/DataStructure/LinkedList.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DataStructure
+﻿namespace DataStructure
 {
+    using System;
     public class LinkedList
     {
         private LinkedListNode mHead;
@@ -36,6 +31,14 @@ namespace DataStructure
         public LinkedListNode Tail
         {
             get { return mTail; }
+        }
+
+        /// <summary>
+        /// Gets the number of nodes<see cref="LinkedList"/>.
+        /// </summary>
+        public int Count
+        {
+            get { return mCount; }
         }
 
         /// Determines whether the <see cref="LinkedListNode"/> is empty.
@@ -95,5 +98,221 @@ namespace DataStructure
 
             mCount++;
         }
+        /// <summary>
+        /// Adds a node after the specified <see cref="LinkedListNode"/> with the value of item.
+        /// </summary>
+        /// <remarks>
+        /// This method is an O(1) operation, the node to add after and new nodes links are updated without having to perform any
+        /// traversal of the linked list.
+        /// </remarks>
+        /// <param name="node">Node in <see cref="LinkedListNode"/> to add node after.</param>
+        /// <param name="item">Item to add to <see cref="LinkedListNode"/>.</param>
+        /// <exception cref="ArgumentNullException"><strong>node</strong> is <strong>null</strong>.</exception>
+        public void AddAfter(LinkedListNode node, int item)
+        {
+
+
+            LinkedListNode n = new LinkedListNode(item);
+
+            // check to see if node is the only node in the linked list
+            if (node == mHead && node == mTail)
+            {
+                mHead.Next = n;
+                mTail = n;
+            }
+            else if (node == mTail)
+            {
+                mTail.Next = n;
+                mTail = n;
+            }
+            else
+            {
+                n.Next = node.Next;
+                node.Next = n;
+            }
+
+            mCount++;
+        }
+        /// <summary>
+        /// Adds a <see cref="LinkedListNode"/> before the specified <see cref="LinkedListNode"/> with the specified value.
+        /// </summary>
+        /// <remarks>
+        /// This method's best case is an O(1) operation where the node to be added before is the <see cref="Head"/> node, otherwise the
+        /// method is an O(n) operation where n is the number of nodes to be traversed in order to find the node before the node to add before.
+        /// </remarks>
+        /// <param name="node">Node in the <see cref="LinkedListNode"/> to add node before.</param>
+        /// <param name="item">Item to add to the <see cref="LinkedListNode"/>.</param>
+        /// <exception cref="ArgumentNullException"><strong>node</strong> is <strong>null</strong>.</exception>
+        public void AddBefore(LinkedListNode node, int item)
+        {
+            if (node == null)
+            {
+                throw new ArgumentNullException("node");
+            }
+
+            LinkedListNode n = new LinkedListNode(item);
+            if (node == mHead)
+            {
+                n.Next = mHead;
+                mHead = n;
+            }
+            else
+            {
+                LinkedListNode curr = mHead;
+                while (curr != null)
+                {
+                    if (curr.Next == node)
+                    {
+                        n.Next = node;
+                        curr.Next = n;
+                        break;
+                    }
+
+                    curr = curr.Next;
+                }
+            }
+
+            mCount++;
+        }
+        /// <summary>
+        /// Removes the last node from the <see cref="LinkedListNode"/>.
+        /// </summary>
+        /// <remarks>
+        /// This method's best case is an O(1) operation when the last node to remove is both the head and tail, i.e. there is only one node
+        /// in the linked list, otherwise the method is an O(n) operation where n nodes have to be traversed in order to locate the node that
+        /// precedes the last node.
+        /// </remarks>
+        /// <returns>True the last node was removed; otherwise false.</returns>
+        public bool RemoveLast()
+        {
+            if (IsEmpty())
+            {
+                return false;
+            }
+
+            if (mCount == 1)
+            {
+                mHead = null;
+                mTail = null;
+            }
+            else
+            {
+                LinkedListNode n = mHead;
+                while (n != null)
+                {
+                    if (n.Next == mTail)
+                    {
+                        mTail = n;
+                        mTail.Next = null;
+                        break;
+                    }
+
+                    n = n.Next;
+                }
+            }
+
+            mCount--;
+            return true;
+        }
+        /// <summary>
+        /// Removes the first node from the <see cref="LinkedListNode"/>.
+        /// </summary>
+        /// <remarks>
+        /// This method is an O(1) operation, the <see cref="Head"/> is always known.
+        /// </remarks>
+        /// <returns>True if the first node was removed; otherwise false.</returns>
+        /// <exception cref="InvalidOperationException"><see cref="LinkedListNode"/> contains <strong>0</strong> items.</exception>
+        public bool RemoveFirst()
+        {
+            if (IsEmpty())
+            {
+                return false;
+            }
+
+            if (mCount == 1)
+            {
+                mHead = null;
+                mTail = null;
+            }
+            else
+            {
+                mHead = mHead.Next;
+            }
+
+            mCount--;
+            return true;
+        }
+        public bool Remove(int item)
+        {
+            if (IsEmpty())
+            {
+                return false;
+            }
+
+            LinkedListNode n = mHead;
+            if (n.Value == item)
+            {
+                // we have one of two cases, either the linked list contains only one node (case 1); or we are removing the head node (case 2)
+                // case 1
+                if (n == mHead && n == mTail)
+                {
+                    mHead = null;
+                    mTail = null;
+                }
+                else
+                {
+                    // case 2
+                    mHead = mHead.Next;
+                }
+
+                mCount--;
+                return true;
+            }
+
+            // case 3: the node to remove is anything but the head node
+            while (n.Next != null && n.Next.Value != item)
+            {
+                n = n.Next;
+            }
+
+            if (n.Next != null && n.Next.Value == item)
+            {
+                // case 3.1: the node to remove is the tail
+                if (n.Next == mTail)
+                {
+                    mTail = n;
+                }
+
+                // case 3.2: the node to remove is somewhere in the linked list, just not the tail node
+                n.Next = n.Next.Next;
+                mCount--;
+                return true;
+            }
+
+            return false; // case 4: item to remove was not in the linked list
+        }
+        /// <summary>
+        /// Determines whether a value is in the <see cref="LinkedList"/>.
+        /// </summary>
+        /// <remarks>
+        /// This method is an O(n) operation where n is the number of nodes in the linked list.
+        /// </remarks>
+        /// <param name="item">Value to search for.</param>
+        /// <returns>True if the value is in the <see cref="LinkedList"/>; false otherwise.</returns>
+        public bool Contains(int item)
+        {
+            LinkedListNode current = mHead;
+            while (current != null)
+            {
+                if (current.Value == item)
+                {
+                    return true;
+                }
+                current = current.Next;
+            }
+            return false;
+        }
+
+
     }
 }
