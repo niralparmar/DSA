cc0a85d449eade47c039e73b1e9b5d75f446729f
c0der_man <niral.parmar@hotmail.com>
2017-05-04 23:27:40 -0700

    New Solutions

--- cs/Algorithms/LinkedList.cs
+++ cs/Algorithms/LinkedList.cs
@@ -14,6 +14,12 @@
             }
             return i;
         }
+        /// <summary>
+        /// https://www.interviewbit.com/problems/intersection-of-linked-lists/
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
         public LinkedListNode GetIntersectionNode(LinkedListNode a, LinkedListNode b)
         {
             int n = GetLinkedListLength(a);
@@ -43,6 +49,15 @@
             }
             return null;
         }
+        /// <summary>
+        /// Reverse a linked list. Do it in-place and in one-pass.
+        ///For example:
+        ///Given 1->2->3->4->5->NULL,
+        ///return 5->4->3->2->1->NULL.
+        ///refer : https://www.interviewbit.com/problems/reverse-linked-list/
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
         public static LinkedListNode ReverseLinkedList(LinkedListNode a)
         {
             LinkedListNode current, prev, next;
@@ -56,6 +71,63 @@
                 current = next;
             }
             return prev;
+        }
+        /// <summary>
+        /// https://www.interviewbit.com/problems/palindrome-list/
+        /// </summary>
+        /// <param name="a"></param>
+        /// <returns></returns>
+        public static bool IsPalindrom(LinkedListNode a)
+        {
+            if (a == null || a.Next == null)
+                return true;
+
+            //find list center
+            LinkedListNode fast = a;
+            LinkedListNode slow = a;
+
+            while (fast.Next != null && fast.Next.Next != null)
+            {
+                fast = fast.Next.Next;
+                slow = slow.Next;
+            }
+
+            LinkedListNode secondHead = slow.Next;
+            slow.Next = null;
+
+            //reverse second part of the list
+            LinkedListNode p1 = secondHead;
+            LinkedListNode p2 = p1.Next;
+
+            while (p1 != null && p2 != null)
+            {
+                LinkedListNode temp = p2.Next;
+                p2.Next = p1;
+                p1 = p2;
+                p2 = temp;
+            }
+
+            secondHead.Next = null;
+
+            //compare two sublists now
+            LinkedListNode p = (p2 == null ? p1 : p2);
+            LinkedListNode q = a;
+            while (p != null)
+            {
+                if (p.Value != q.Value)
+                    return false;
+
+                p = p.Next;
+                q = q.Next;
+
+            }
+
+            return true;
+        }
+        public static LinkedListNode RemoveDuplicate(LinkedListNode a)
+        {
+
+            return new LinkedListNode(3);
         }
     }
 }
--- cs/DSATest/AlgorithmTest/LinkedListTest.cs
+++ cs/DSATest/AlgorithmTest/LinkedListTest.cs
@@ -18,5 +18,16 @@
             Assert.AreEqual(node.Value, 4);
 
         }
+        [TestMethod]
+        public void Palindromtest()
+        {
+            DataStructure.LinkedList list = new DataStructure.LinkedList();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(1);
+            Assert.AreEqual(Algorithms.LinkedList.IsPalindrom(list.Head), false);
+
+        }
     }
 }
